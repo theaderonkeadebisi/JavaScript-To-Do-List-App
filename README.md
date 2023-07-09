c58308b63a6682bf46a13f9e7ae894e1a8f74d7f
theaderonkeadebisi <theaderonkeadebisi@gmail.com>
2023-07-09 14:45:20 -0700

    Creates a to-do list

---
+++ README.md
@@ -0,0 +1,7 @@
+# Project Title
+
+My To-Do List App
+
+## Description
+
+My To-Do list App is a web application that allows users to make a list of things they want to do. It provides a user-friendly interface and helpful features to help users remember things and stay organized.
---
+++ todoApp.js
@@ -0,0 +1,96 @@
+const taskInput = document.querySelector('#taskInput')
+const taskList = document.querySelector('#taskList')
+const addTaskForm = document.querySelector('#addTaskForm')
+const clearAllButton = document.querySelector('#clearAllButton')
+const markAllButton = document.querySelector('#markAllButton')
+
+taskList.style = `
+list-style: none;
+margin-top: 0.7rem;
+font-size: 1.5rem
+`
+
+
+const createTaskItem = (task) => `
+<li>
+    <input type="checkbox" name="task" value="$(task)" onChange="toggleTaskCompletion(event)">
+    <label for="task">${task}</label>
+    <button type="button" onClick="removeTask(event)">
+    X
+    </button>
+</li>
+`
+const storedTasks = 
+    JSON.parse(localStorage.getItem('tasks')) || []
+const renderTasks = ()  => {
+    storedTasks.forEach((task) => {
+        taskList.insertAdjacentHTML(
+            'beforeend',
+            createTaskItem(task)
+        )
+    })
+}
+
+window.onload = renderTasks
+
+const addTask = (event) => {
+    event.preventDefault()
+
+    const task = taskInput.value
+    const taskItem = createTaskItem(task)
+    taskList.insertAdjacentHTML('beforeend', taskItem)
+
+    storedTasks.push(task)
+    localStorage.setItem(
+        'tasks',
+        JSON.stringify(storedTasks)
+    )
+
+    addTaskForm.reset()
+}
+
+addTaskForm.addEventListener('submit', addTask)
+
+const toggleTaskCompletion = (event) => {
+    const taskItem = event.target.parentElement
+    const task = taskItem.querySelector('label')
+
+    if (event.target.checked) {
+        task.style.textDecoration = 'line-through'
+    } else {
+        task.style.textDecoration = 'none'
+    }
+}
+
+const removeTask = (event) => {
+    const taskItem = event.target.parentElement
+    const task = taskItem.querySelector('label').innerText
+    const indexOfTask = storedTasks.indexOf(task)
+    storedTasks.splice(indexOfTask, 1)
+    localStorage.setItem(
+        'tasks',
+        JSON.stringify(storedTasks)
+    )
+
+    taskItem.remove()
+}
+
+
+const clearAllTasks = () => {
+    taskList.innerHTML = ''
+    storedTasks.length = 0
+    localStorage.removeItem('tasks')
+};
+
+const markAllTasks = () => {
+    const checkboxes = taskList.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach((checkbox) => {
+        checkbox.checked = true;
+        const taskItem = checkbox.parentElement
+        const taskLabel = taskItem.querySelector('label')
+        taskLabel.style.textDecoration = 'line-through'
+    })
+}
+
+clearAllButton.addEventListener('click', clearAllTasks)
+markAllButton.addEventListener('click', markAllTasks)
